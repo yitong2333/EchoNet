dd93da34e70649f7d90978633fd12891c72f38db
yitong <yitong@>
2024-05-16 23:56:50 +0800

    添加项目文件。

---
+++ TCPServerNET/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace TCPServerNET
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIpPort = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.cmbIpAddr = new System.Windows.Forms.ComboBox();
+            this.cmbProtocol = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rtxRec = new System.Windows.Forms.RichTextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtRemotePort = new System.Windows.Forms.TextBox();
+            this.txtRemoteIp = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.chkTxHex = new System.Windows.Forms.CheckBox();
+            this.chkRxHex = new System.Windows.Forms.CheckBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.rtxSend = new System.Windows.Forms.RichTextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtIpPort);
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Controls.Add(this.cmbIpAddr);
+            this.groupBox1.Controls.Add(this.cmbProtocol);
+            this.groupBox1.Location = new System.Drawing.Point(15, 14);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(189, 219);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "网络设置";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "端口";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "IP";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "协议";
+            // 
+            // txtIpPort
+            // 
+            this.txtIpPort.Location = new System.Drawing.Point(53, 110);
+            this.txtIpPort.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtIpPort.Name = "txtIpPort";
+            this.txtIpPort.Size = new System.Drawing.Size(108, 25);
+            this.txtIpPort.TabIndex = 3;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(53, 141);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(109, 39);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "连接";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.connectBtn_Click);
+            // 
+            // cmbIpAddr
+            // 
+            this.cmbIpAddr.FormattingEnabled = true;
+            this.cmbIpAddr.Location = new System.Drawing.Point(53, 66);
+            this.cmbIpAddr.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmbIpAddr.Name = "cmbIpAddr";
+            this.cmbIpAddr.Size = new System.Drawing.Size(108, 23);
+            this.cmbIpAddr.TabIndex = 1;
+            this.cmbIpAddr.Text = "127.0.0.1";
+            // 
+            // cmbProtocol
+            // 
+            this.cmbProtocol.FormattingEnabled = true;
+            this.cmbProtocol.Location = new System.Drawing.Point(53, 24);
+            this.cmbProtocol.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmbProtocol.Name = "cmbProtocol";
+            this.cmbProtocol.Size = new System.Drawing.Size(108, 23);
+            this.cmbProtocol.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rtxRec);
+            this.groupBox2.Location = new System.Drawing.Point(209, 14);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Size = new System.Drawing.Size(481, 275);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "接收区";
+            // 
+            // rtxRec
+            // 
+            this.rtxRec.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtxRec.Location = new System.Drawing.Point(3, 20);
+            this.rtxRec.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rtxRec.Name = "rtxRec";
+            this.rtxRec.Size = new System.Drawing.Size(475, 253);
+            this.rtxRec.TabIndex = 0;
+            this.rtxRec.Text = "";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtRemotePort);
+            this.groupBox3.Controls.Add(this.txtRemoteIp);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Location = new System.Drawing.Point(15, 240);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox3.Size = new System.Drawing.Size(189, 102);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "远程主机设置";
+            // 
+            // txtRemotePort
+            // 
+            this.txtRemotePort.Location = new System.Drawing.Point(53, 56);
+            this.txtRemotePort.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtRemotePort.Name = "txtRemotePort";
+            this.txtRemotePort.Size = new System.Drawing.Size(107, 25);
+            this.txtRemotePort.TabIndex = 3;
+            // 
+            // txtRemoteIp
+            // 
+            this.txtRemoteIp.Location = new System.Drawing.Point(53, 22);
+            this.txtRemoteIp.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtRemoteIp.Name = "txtRemoteIp";
+            this.txtRemoteIp.Size = new System.Drawing.Size(108, 25);
+            this.txtRemoteIp.TabIndex = 2;
+            this.txtRemoteIp.Text = "127.0.0.1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 60);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 15);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "端口";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "IP";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.chkTxHex);
+            this.groupBox4.Controls.Add(this.chkRxHex);
+            this.groupBox4.Location = new System.Drawing.Point(15, 348);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox4.Size = new System.Drawing.Size(189, 85);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "其他设置";
+            // 
+            // chkTxHex
+            // 
+            this.chkTxHex.AutoSize = true;
+            this.chkTxHex.Location = new System.Drawing.Point(91, 42);
+            this.chkTxHex.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.chkTxHex.Name = "chkTxHex";
+            this.chkTxHex.Size = new System.Drawing.Size(69, 19);
+            this.chkTxHex.TabIndex = 3;
+            this.chkTxHex.Text = "Txhex";
+            this.chkTxHex.UseVisualStyleBackColor = true;
+            this.chkTxHex.CheckedChanged += new System.EventHandler(this.chkTxHex_CheckedChanged);
+            // 
+            // chkRxHex
+            // 
+            this.chkRxHex.AutoSize = true;
+            this.chkRxHex.Location = new System.Drawing.Point(13, 42);
+            this.chkRxHex.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.chkRxHex.Name = "chkRxHex";
+            this.chkRxHex.Size = new System.Drawing.Size(69, 19);
+            this.chkRxHex.TabIndex = 2;
+            this.chkRxHex.Text = "RxHex";
+            this.chkRxHex.UseVisualStyleBackColor = true;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.rtxSend);
+            this.groupBox5.Location = new System.Drawing.Point(212, 291);
+            this.groupBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox5.Size = new System.Drawing.Size(409, 141);
+            this.groupBox5.TabIndex = 4;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "发送区";
+            // 
+            // rtxSend
+            // 
+            this.rtxSend.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtxSend.Location = new System.Drawing.Point(3, 20);
+            this.rtxSend.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rtxSend.Name = "rtxSend";
+            this.rtxSend.Size = new System.Drawing.Size(403, 119);
+            this.rtxSend.TabIndex = 0;
+            this.rtxSend.Text = "";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(627, 311);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(64, 121);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 448);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "网络调试助手 by：yitong";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIpPort;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ComboBox cmbIpAddr;
+        private System.Windows.Forms.ComboBox cmbProtocol;
+        private System.Windows.Forms.RichTextBox rtxRec;
+        private System.Windows.Forms.TextBox txtRemotePort;
+        private System.Windows.Forms.TextBox txtRemoteIp;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox rtxSend;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.CheckBox chkTxHex;
+        private System.Windows.Forms.CheckBox chkRxHex;
+    }
+}
+
---
+++ TCPServerNET/Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TCPServerNET
+{
+    public partial class Form1 : Form
+    {
+        private List<IPAddress> ipList = new List<IPAddress>();
+        private List<TcpClient> tcplists = new List<TcpClient>();
+        private TcpListener tcpListener;
+        private Thread listenThread;
+        private UdpClient udpClient;
+        private bool isConnected = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbProtocol.Items.AddRange(new string[] { "UDP", "TCP Server", "TCP Client" });
+            cmbProtocol.SelectedIndex = 2;  // Default to TCP Client
+
+            ipList = GetIPAddresses();
+            if (ipList.Count > 0)
+            {
+                cmbIpAddr.Items.AddRange(ipList.ToArray());
+                cmbIpAddr.SelectedIndex = 0;
+            }
+            txtIpPort.Text = "25565";
+            UpdateConnectionControls(false);
+        }
+
+        private List<IPAddress> GetIPAddresses()
+        {
+            var addresses = new List<IPAddress>();
+            foreach (NetworkInterface intf in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                foreach (UnicastIPAddressInformation ip in intf.GetIPProperties().UnicastAddresses)
+                {
+                    if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        addresses.Add(ip.Address);
+                    }
+                }
+            }
+            return addresses;
+        }
+
+        private void connectBtn_Click(object sender, EventArgs e)
+        {
+            if (!isConnected)
+            {
+                StartConnection();
+            }
+            else
+            {
+                StopConnection();
+            }
+        }
+
+        private void StartConnection()
+        {
+            int port = int.Parse(txtIpPort.Text);
+            switch (cmbProtocol.SelectedItem.ToString())
+            {
+                case "TCP Server":
+                    tcpListener = new TcpListener(IPAddress.Parse(cmbIpAddr.Text), port);
+                    tcpListener.Start();
+                    listenThread = new Thread(TCPReceiveData);
+                    listenThread.IsBackground = true;
+                    listenThread.Start();
+                    break;
+                case "TCP Client":
+                    var client = new TcpClient();
+                    client.Connect(IPAddress.Parse(cmbIpAddr.Text), port);
+                    tcplists.Add(client);
+                    break;
+                case "UDP":
+                    udpClient = new UdpClient(port);
+                    listenThread = new Thread(UdpReceiveData);
+                    listenThread.IsBackground = true;
+                    listenThread.Start();
+                    break;
+            }
+            isConnected = true;
+            UpdateConnectionControls(true);
+        }
+
+
+        private void StopConnection()
+        {
+            if (tcpListener != null)
+            {
+                tcpListener.Stop();
+                tcpListener = null;
+            }
+            if (listenThread != null)
+            {
+                listenThread.Abort();
+                listenThread = null;
+            }
+            foreach (var client in tcplists)
+            {
+                client.Close();
+            }
+            tcplists.Clear();
+            if (udpClient != null)
+            {
+                udpClient.Close();
+                udpClient = null;
+            }
+            isConnected = false;
+            UpdateConnectionControls(false);
+        }
+
+        private void UpdateConnectionControls(bool isConnected)
+        {
+            btnConnect.Text = isConnected ? "断开" : "连接";
+            cmbIpAddr.Enabled = !isConnected;
+            txtIpPort.Enabled = !isConnected;
+            cmbProtocol.Enabled = !isConnected;
+        }
+        private void UdpReceiveData()
+        {
+            try
+            {
+                IPEndPoint remoteEP = null;
+                while (true)
+                {
+                    byte[] data = udpClient.Receive(ref remoteEP);
+                    string message = Encoding.UTF8.GetString(data);
+                    UpdateUI(() =>
+                    {
+                        rtxRec.SelectionColor = Color.Blue;
+                        rtxRec.AppendText("[REC UDP]: " + message + "\n");
+                        rtxRec.ScrollToCaret();
+                    });
+                }
+            }
+            catch (ThreadAbortException)
+            {
+                // Handle thread abort for clean exit
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("接收UDP数据时发生错误：" + ex.Message);
+            }
+        }
+        private void TCPReceiveData()
+        {
+            while (true)
+            {
+                if (tcpListener != null && tcpListener.Pending())
+                {
+                    var client = tcpListener.AcceptTcpClient();
+                    tcplists.Add(client);
+                    Thread receiveThread = new Thread(() => ProcessReceivedData(client));
+                    receiveThread.IsBackground = true;
+                    receiveThread.Start();
+                }
+            }
+        }
+
+        private void ProcessReceivedData(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];  // 可以根据实际情况调整缓冲区大小
+            int bytesRead;
+
+            try
+            {
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    var data = new byte[bytesRead];
+                    Array.Copy(buffer, data, bytesRead);
+
+                    // 判断是否以十六进制显示
+                    string message;
+                    if (chkRxHex.Checked)
+                    {
+                        message = BitConverter.ToString(data).Replace("-", " ");  // 将字节转换为十六进制字符串
+                    }
+                    else
+                    {
+                        message = Encoding.UTF8.GetString(data);  // 将字节转换为字符串
+                    }
+
+                    string formattedMessage = "[REC] " + message + "\n";
+
+                    // 在UI线程上更新富文本框
+                    UpdateUI(() => {
+                        rtxRec.SelectionColor = Color.Blue;  // 设置文本颜色
+                        rtxRec.AppendText(formattedMessage);
+                        rtxRec.ScrollToCaret();  // 滚动到新添加的文本位置
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("接收数据时发生错误：" + ex.Message);
+            }
+        }
+
+
+
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string message = rtxSend.Text.Trim();
+            if (string.IsNullOrEmpty(message))
+            {
+                MessageBox.Show("请输入要发送的数据");
+                return;
+            }
+
+            byte[] data;
+            if (chkTxHex.Checked)
+            {
+                try
+                {
+                    data = ConvertHexStringToByteArray(message);
+                }
+                catch (FormatException fe)
+                {
+                    MessageBox.Show("输入的十六进制字符串格式不正确：" + fe.Message);
+                    return;
+                }
+            }
+            else
+            {
+                data = Encoding.UTF8.GetBytes(message);
+            }
+
+            switch (cmbProtocol.SelectedItem.ToString())
+            {
+                case "TCP Server":
+                case "TCP Client":
+                    SendTcpData(data);
+                    break;
+                case "UDP":
+                    SendUdpData(data);
+                    break;
+            }
+        }
+        private void SendTcpData(byte[] data)
+        {
+            foreach (var client in tcplists)
+            {
+                try
+                {
+                    NetworkStream stream = client.GetStream();
+                    if (stream.CanWrite)
+                    {
+                        stream.Write(data, 0, data.Length);
+                        UpdateUI(() =>
+                        {
+                            rtxRec.SelectionColor = Color.Green;
+                            rtxRec.AppendText("[SENT TCP]: " + Encoding.UTF8.GetString(data) + "\n");
+                            rtxRec.ScrollToCaret();
+                        });
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("发送TCP数据出错：" + ex.Message);
+                }
+            }
+        }
+
+
+        private void SendUdpData(byte[] data)
+        {
+            try
+            {
+                if (udpClient != null)
+                {
+                    // Assuming txtRemoteIp and txtRemotePort contain the remote endpoint information
+                    IPAddress remoteIp = IPAddress.Parse(txtRemoteIp.Text);
+                    int remotePort = int.Parse(txtRemotePort.Text);
+                    IPEndPoint remoteEndPoint = new IPEndPoint(remoteIp, remotePort);
+
+                    udpClient.Send(data, data.Length, remoteEndPoint);  // Send data to specified endpoint
+                    UpdateUI(() =>
+                    {
+                        rtxRec.SelectionColor = Color.Green;
+                        rtxRec.AppendText("[SENT UDP]: " + Encoding.UTF8.GetString(data) + "\n");
+                        rtxRec.ScrollToCaret();
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("发送UDP数据出错：" + ex.Message);
+            }
+        }
+
+        private void UpdateUI(Action updateAction)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(updateAction);
+            }
+            else
+            {
+                updateAction();
+            }
+        }
+
+        private byte[] ConvertHexStringToByteArray(string hexString)
+        {
+            try
+            {
+                int numberChars = hexString.Length;
+                byte[] bytes = new byte[numberChars / 2];
+                for (int i = 0; i < numberChars; i += 2)
+                {
+                    bytes[i / 2] = Convert.ToByte(hexString.Substring(i, 2), 16);
+                }
+                return bytes;
+            }
+            catch
+            {
+                MessageBox.Show("非法的 hex string");
+                return new byte[] { };
+            }
+        }
+
+        private void chkTxHex_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkTxHex.Checked)
+            {
+                string text = rtxSend.Text;
+                byte[] bytes = Encoding.UTF8.GetBytes(text);
+                string hex = BitConverter.ToString(bytes).Replace("-", " ");
+                rtxSend.Text = hex;
+            }
+            else
+            {
+                string hex = rtxSend.Text;
+                byte[] bytes = ConvertHexStringToByteArray(hex);
+                string text = Encoding.UTF8.GetString(bytes);
+                rtxSend.Text = text;
+            }
+        }
+    }
+}
